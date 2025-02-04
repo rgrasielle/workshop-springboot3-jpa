d037b61e68b5ce90205e94bdb594571d2541ab4f
rgrasielle <rgrasielle1@gmail.com>
2025-02-04 15:34:46 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,6 +38,9 @@ public class Product implements Serializable {
 	inverseJoinColumns = @JoinColumn(name = "category_id"))  // define a chave estrangeira da outra entidade (Category)
 	private Set<Category> categories = new HashSet<>();   // garante que o mesmo produto não tenha a mesma categoria mais de uma vez
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	// Construtores
 	public Product() {
 	}
@@ -93,7 +99,16 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder()); 
+		}
+		return set;
+	}
+ 
 	// HashCode e Equals (somente id)
 	@Override
 	public int hashCode() {
